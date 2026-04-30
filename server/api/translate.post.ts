cc204de1d053fb2514df3843cc58958467ab5ba0
alinapantina <animadelirious@gmail.com>
2026-04-30 18:51:17 +0100

    api translate conflict test

--- server/api/translate.post.ts
+++ server/api/translate.post.ts
@@ -31,6 +31,15 @@
 import { v2 } from '@google-cloud/translate'
 const { Translate } = v2
 
+
+const credentials = JSON.parse(
+  process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON!
+)
+
+const translate = new Translate({
+  credentials
+})
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
   const { text, target, source } = body
@@ -38,37 +47,56 @@ export default defineEventHandler(async (event) => {
   if (!text) {
     throw createError({
       statusCode: 400,
-      statusMessage: 'No text provided'
+      statusMessage: 'No text provided',
     })
   }
 
-  const keyPath = process.env.GOOGLE_APPLICATION_CREDENTIALS
+ const [translated] = await translate.translate(text, {
+    from: source || 'uk',
+    to: target || 'ru'
+  })
 
-  if (!keyPath) {
-    throw createError({
-      statusCode: 500,
-      statusMessage: 'GOOGLE_APPLICATION_CREDENTIALS is not set'
-    })
-  }
-
-  try {
-    const translate = new Translate({
-      keyFilename: keyPath
-    })
-
-    const [translated] = await translate.translate(text, {
-      from: source || 'uk',
-      to: target || 'ru'
-    })
-
-    return { translated }
-  } catch (err: any) {
-    console.error('Translate error:', err)
-
-    throw createError({
-      statusCode: 500,
-      statusMessage: 'Translation failed',
-      data: err.message
-    })
-  }
+  return { translated }
 })
+
+// export default defineEventHandler(async (event) => {
+//   const body = await readBody(event)
+//   const { text, target, source } = body
+
+//   if (!text) {
+//     throw createError({
+//       statusCode: 400,
+//       statusMessage: 'No text provided'
+//     })
+//   }
+
+//   const keyPath = process.env.GOOGLE_APPLICATION_CREDENTIALS
+
+//   if (!keyPath) {
+//     throw createError({
+//       statusCode: 500,
+//       statusMessage: 'GOOGLE_APPLICATION_CREDENTIALS is not set'
+//     })
+//   }
+
+//   try {
+//     const translate = new Translate({
+//       keyFilename: keyPath
+//     })
+
+//     const [translated] = await translate.translate(text, {
+//       from: source || 'uk',
+//       to: target || 'ru'
+//     })
+
+//     return { translated }
+//   } catch (err: any) {
+//     console.error('Translate error:', err)
+
+//     throw createError({
+//       statusCode: 500,
+//       statusMessage: 'Translation failed',
+//       data: err.message
+//     })
+//   }
+// })
