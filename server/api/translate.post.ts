943bc6ba06996c6683b4864624fad611510d54c5
alinapantina <animadelirious@gmail.com>
2026-04-30 11:23:17 +0100

    api translate conflict test

--- server/api/translate.post.ts
+++ server/api/translate.post.ts
@@ -1,32 +1,5 @@
-// import { Translate } from '@google-cloud/translate/build/src/v2'
-
-// const credentials = JSON.parse(
-//   process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON!
-// )
-
-// const translate = new Translate({
-//   credentials
-// })
-
-// export default defineEventHandler(async (event) => {
-//   const body = await readBody(event)
-//   const { text, target, source } = body
-
-//   if (!text) {
-//     throw createError({
-//       statusCode: 400,
-//       statusMessage: 'No text provided',
-//     })
-//   }
-
-//  const [translated] = await translate.translate(text, {
-//     from: source || 'uk',
-//     to: target || 'ru'
-//   })
-
-//   return { translated }
-// })
-import { Translate } from '@google-cloud/translate/build/src/v2'
+import { v2 } from '@google-cloud/translate'
+const { Translate } = v2
 
 export default defineEventHandler(async (event) => {
   const body = await readBody(event)
